f2cac00228b2e1eb2ffd173e526eef5808f623b4
aalmarzou <hamani7115@gmail.com>
2023-09-20 15:13:23 +0300

    initial commit'

---
+++ my-weather-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation/>
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import { RouterView } from 'vue-router';
+import SiteNavigation  from "./components/SiteNavigation.vue"
+
+export default {
+    setup() {
+        return {};
+    },
+    components: { 
+      RouterView,
+      SiteNavigation
+     }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
